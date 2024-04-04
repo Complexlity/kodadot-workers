b356a5216e71ec2e796df44c307d80c7bad47dd7
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-04-04 16:44:01 +0100

    perf: fetch first item

--- collections-frame/src/routes/gallery.tsx
+++ collections-frame/src/routes/gallery.tsx
@@ -9,10 +9,11 @@ export const app = new Frog<HonoEnv>({});
 app.frame("/:chain/:id", async (c) => {
   const { chain, id } = c.req.param();
 
-  const collection = await getCollection(chain, id);
+  const [collection, firstItemInCollection] = await Promise.all([getCollection(chain, id), getItemByOffset(chain, id, 0)]);
+  console.log({firstItemInCollection})
+  const firstItemId = firstItemInCollection.id.split('-')[1]
+  
 
-  // console.log({collectionItems})
-  // console.log({ collection });
 
   const image = $purifyOne(collection.image, "kodadot_beta");
   const max = collection.max;
@@ -25,7 +26,7 @@ app.frame("/:chain/:id", async (c) => {
     image,
     imageAspectRatio: "1:1",
     intents: [
-      <Button action={`/view/${chain}/${id}/1`} value={supply}>
+      <Button action={`/view/${chain}/${id}/${firstItemId}`} value={supply}>
         {label}
       </Button>,
     ],
